80a87e6fd83cbed75580ac6cb36c86ca8fed50f2
luciomelis <luciomelis1992@gmail.com>
2022-05-04 16:13:32 +0200

    sono riuscito a stampare anche la risposta

--- index.php
+++ index.php
@@ -5,8 +5,12 @@ include('array.php');
 // echo('</pre>');
 foreach ($arrayFaq as $key => $item) {
     echo('<h2>' . $item['domanda'] . '</h2>');
-    foreach ($item['risposta'] as $risposta){
-        echo('<p>' . explode('/', $risposta) . '</p>');
+   $itemRisposta = explode('/', $item['risposta']);
+    echo('<pre>');
+    var_dump($itemRisposta);
+    echo('</pre>');
+    foreach($itemRisposta as $paragrafo){
+        echo('<p>' . $paragrafo . '</p>');
     }
 }
     
