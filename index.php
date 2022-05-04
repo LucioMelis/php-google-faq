57262dc313c5659db5ad77e623fcea8db6d1dc8a
luciomelis <luciomelis1992@gmail.com>
2022-05-04 15:55:01 +0200

    cerco di utilizzare il metodo explode

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-phpinfo();
+include('array.php');
+// echo('<pre>');
+// var_dump($arrayFaq);
+// echo('</pre>');
+foreach ($arrayFaq as $key => $item) {
+    echo('<h2>' . $item['domanda'] . '</h2>');
+    foreach ($item['risposta'] as $risposta){
+        echo('<p>' . explode('/', $risposta) . '</p>');
+    }
+}
+    
+
 ?>
